ef5a0140de187224b00094f690e8833f7cd704e3
asahi <asahi@ubuntu>
2023-04-28 08:15:01 +0000

    feat: add plugins to linked dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -95,7 +95,7 @@ end
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-append :linked_dirs, "files", "tmp/pdf", "public/plugin_assets", "log", "tmp/pids", "tmp/cache", "tmp/sockets"
+append :linked_dirs, "plugins", "files", "tmp/pdf", "public/plugin_assets", "log", "tmp/pids", "tmp/cache", "tmp/sockets"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
